f855b195ef8661075f6f56dc28009db8358ccb74
Priyanshu Shukla <145186452+Priyansh666216@users.noreply.github.com>
2026-06-16 12:48:13 +0530

    Delete Module1-DesignPatterns directory

--- Module1-DesignPatterns/SingletonPatternExample/AppService.java
+++
@@ -1,9 +0,0 @@
-package SingletonPatternExample;
-
-public class AppService {
-
-    public void doWork() {
-        Logger logger = Logger.getInstance();
-        logger.info("AppService is doing some work...");
-    }
-}
